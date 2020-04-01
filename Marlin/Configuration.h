59a779d046269b8c759955a18955abba7a2b4013
Viktor <viktor.lis92@gmail.com>
2020-04-01 10:00:44 +0300

    EXTRUDE_MAXLENGTH 1100

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -565,7 +565,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 200
+#define EXTRUDE_MAXLENGTH 1100
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
